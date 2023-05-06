7cc23ba436aa3df2b558b47a1a80fb048b98ee77
Isaiah Jones <injones27@gmail.com>
2023-05-06 17:51:28 -0400

    Phase-1 Array Filter Method Lab Completed!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, stringName) {
+    return drivers.filter((driver)=> {
+        return driver.toLowerCase() === stringName.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, stringLetters){
+    return drivers.filter((driver)=> {
+        return driver.charAt(0) === stringLetters.charAt(0)
+    })
+}
+
+function matchName(drivers, stringName) {
+    return drivers.filter((driver)=>{
+        return driver.name === stringName
+    })
+}
